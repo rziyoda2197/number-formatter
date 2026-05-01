04012787e2273fc89fbaea0f3f93b6fb59b74f18
rziyoda2197 <rziyoda2197@gmail.com>
2026-05-01 15:57:35 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,8 @@
+def format_number(num):
+    return "{:,}".format(num)
+```
+
+Kodni ishlatish uchun misol:
+```python
+print(format_number(1000000))  # Chiqaradi: 1,000,000
+print(format_number(1234567))  # Chiqaradi: 1,234,567
